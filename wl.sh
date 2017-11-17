62b3feeb2d4894b76c1862eaf1a0df46c33104c4
Shin-ichiro Naito <shinichiro.555@icloud.com>
2017-11-18 05:11:26 +0900

    update

--- wl.sh
+++ wl.sh
@@ -19,8 +19,9 @@ cd /tmp/broadcom
 # Download some support files from the Arch package manually ~/Downloads
 # https://aur.archlinux.org/cgit/aur.git/snapshot/broadcom-wl-dkms.tar.gz
 
-cp ./wl/broadcom-wl-dkms.tar.gz /tmp/broadcom/
-cp ./wl/hybrid-v35_64-nodebug-pcoem-6_30_223_271.tar.gz /tmp/broadcom/broadcom.tar.gz
+"${base%/}/${rel}"
+cp "${dirname $0%}/${wl/broadcom-wl-dkms.tar.gz}" /tmp/broadcom/
+cp "${dirname $0%}/${wl/hybrid-v35_64-nodebug-pcoem-6_30_223_271.tar.gz}" /tmp/broadcom/broadcom.tar.gz
 
 
 # Untar the archives
